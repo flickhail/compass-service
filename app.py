c7a2ba4a864d33b091f7054adf6ce9c0e25c78c0
german <german@Ubuntu.myguest.virtualbox.org>
2024-04-03 22:21:18 +0500

    added reverse proxy dockerfile

--- app.py
+++ app.py
@@ -4,12 +4,13 @@ import flask
 from flask import Flask
 
 from dotenv import load_dotenv
+
 load_dotenv()
 
 import os
 
 DEFAULT_IP_ADRESS = '127.0.0.1'
-DEFAULT_PORT = '5000'
+DEFAULT_PORT = '2000'
 
 app = Flask(__name__)
 
@@ -18,6 +19,10 @@ def index():
 	return flask.render_template('index.html')
 
 if __name__ == '__main__':
-	app.run(debug=True, host=os.getenv('IP_ADRESS', DEFAULT_IP_ADRESS),   \
-		  port=os.getenv('PORT', DEFAULT_PORT))
+	#app.run(debug=True, host=os.getenv('IP_ADRESS', DEFAULT_IP_ADRESS),   \
+	#	  port=os.getenv('PORT', DEFAULT_PORT))
+	print(os.getenv('IP_ADRESS', DEFAULT_IP_ADRESS))
+	print(os.getenv('PORT', DEFAULT_PORT))
+	app.run(debug=True, host=DEFAULT_IP_ADRESS,   \
+		  port=DEFAULT_PORT)
 
